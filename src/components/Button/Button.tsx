5f49bbff23ed3c34164702aef7f69fbec540c228
Сергей Викторович Князев <https://github.com/Knyazew36>
2023-06-05 00:02:17 +0300

    Up

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,19 @@ const Button: React.FC<IButtonProps> = ({
   handleButtonClick,
   buttonIsActive,
 }) => {
+  const initialClass =
+    'flex items-center gap-1  cursor-pointer text-[18px] text-button-color';
   return (
     <button
       type='submit'
       onClick={handleButtonClick}
-      className='flex items-center gap-1  cursor-pointer text-[18px] text-button-color'
+      className={
+        buttonIsActive
+          ? initialClass + ' bg-[#0079BF] text-white px-2 rounded-md'
+          : initialClass
+      }
     >
-      <img src='Plus.svg'></img>
+      <img src='Plus.svg' className={buttonIsActive ? 'hidden' : ''} />
       {buttonIsActive ? 'Submit' : 'Add card'}
     </button>
   );
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,12 @@
+import useCategory from '../../hooks/useCategory';
+
 const Footer = () => {
+  const data = useCategory('finished');
   return (
     <footer className='flex justify-between w-full px-5 py-[8px] items-center bg-[#0067A3] shadow-md'>
       <div className='flex gap-[36px]'>
         <p className='text-[18px] text-white'>Active tasks: </p>
-        <p className='text-[18px] text-white'>Finished tasks: </p>
+        <p className='text-[18px] text-white'>Finished tasks: {data.length} </p>
       </div>
       <div>
         <p className='text-[18px] text-white'>Kanban board by </p>
